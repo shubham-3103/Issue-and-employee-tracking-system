34403d5983fb7de88fb2a3f2510f093093d3e78c
Shubham <shubhamsharma31031991@gmail.com>
2022-07-14 09:19:34 +0530

    front nearly completed

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -82,8 +82,8 @@
    */
 
   var TRANSITION_END = 'transitionend';
-  var MAX_UID = 1000000;
-  var MILLISECONDS_MULTIPLIER = 1000; // Shoutout AngusCroll (https://goo.gl/pxwQGp)
+  var MAX_UID = 10000;
+  var MILLISECONDS_MULTIPLIER = 1000;
 
   function toType(obj) {
     return {}.toString.call(obj).match(/\s([a-z]+)/i)[1].toLowerCase();
